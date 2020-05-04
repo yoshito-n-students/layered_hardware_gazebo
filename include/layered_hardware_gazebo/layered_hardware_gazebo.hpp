5655d745a7fb1354c721de6c854c6079b91b8dc9
Yoshito Okada <okada@rm.is.tohoku.ac.jp>
2020-05-04 15:04:09 -0700

    use LayeredHardware as a parent class of LayeredHardwareGazebo

--- include/layered_hardware_gazebo/layered_hardware_gazebo.hpp
+++ include/layered_hardware_gazebo/layered_hardware_gazebo.hpp
@@ -1,51 +1,35 @@
 #ifndef LAYERED_HARDWARE_GAZEBO_LAYERED_HARDWARE_GAZEBO_HPP
 #define LAYERED_HARDWARE_GAZEBO_LAYERED_HARDWARE_GAZEBO_HPP
 
-#include <list>
 #include <string>
 #include <vector>
 
-#include <hardware_interface/controller_info.h>
-#include <hardware_interface/robot_hw.h>
 #include <layered_hardware/layer_base.hpp>
+#include <layered_hardware/layered_hardware.hpp>
 #include <layered_hardware_gazebo/common_namespaces.hpp>
 #include <layered_hardware_gazebo/gazebo_layer_base.hpp>
 #include <pluginlib/class_loader.hpp>
 #include <ros/console.h>
-#include <ros/duration.h>
-#include <ros/names.h>
 #include <ros/node_handle.h>
-#include <ros/param.h>
-#include <ros/time.h>
 
 #include <gazebo/physics/physics.hh>
 
-#include <boost/foreach.hpp>
-
 namespace layered_hardware_gazebo {
 
-class LayeredHardwareGazebo : public hi::RobotHW {
+class LayeredHardwareGazebo : public lh::LayeredHardware {
 public:
   LayeredHardwareGazebo()
-      : layer_loader_("layered_hardware", "layered_hardware::LayerBase"),
-        gazebo_layer_loader_("layered_hardware_gazebo",
-                             "layered_hardware_gazebo::GazeboLayerBase") {}
+      : lh::LayeredHardware(), gazebo_layer_loader_("layered_hardware_gazebo",
+                                                    "layered_hardware_gazebo::GazeboLayerBase") {}
 
   virtual ~LayeredHardwareGazebo() {}
 
   bool init(const ros::NodeHandle &param_nh, const gzp::ModelPtr model) {
-    namespace rn = ros::names;
-    namespace rp = ros::param;
-
     // get URDF description from param
-    std::string urdf_str;
-    if (!param_nh.getParam("robot_description", urdf_str) &&
-        !rp::get("robot_description", urdf_str)) {
-      ROS_WARN_STREAM(
-          "LayeredHardwareGazebo::init(): Failed to get URDF description from params neither '"
-          << param_nh.resolveName("robot_description") << "' nor '"
-          << rn::resolve("robot_description")
-          << "'. Every layers will be initialized with empty string.");
+    const std::string urdf_str(getURDFStr(param_nh));
+    if (urdf_str.empty()) {
+      ROS_WARN("LayeredHardwareGazebo::init(): Failed to get URDF description. "
+               "Every layers will be initialized with empty string.");
       // continue because layers may not require robot description
     }
 
@@ -79,43 +63,21 @@ public:
 
       // load as a gazebo layer
       if (gazebo_layer_loader_.isClassAvailable(lookup_name)) {
-        GazeboLayerPtr gazebo_layer;
-        // create a gazebo layer instance by typename
-        try {
-          gazebo_layer = gazebo_layer_loader_.createInstance(lookup_name);
-        } catch (const pluginlib::PluginlibException &ex) {
-          ROS_ERROR_STREAM("LayeredHardwareGazebo::init(): Failed to create a gazebo layer "
-                           "instance by the lookup name '"
-                           << lookup_name << "' for the gazebo layer '" << layer_name
-                           << "': " << ex.what());
-          return false;
-        }
-        // init the layer
-        if (!gazebo_layer->init(this, layer_param_nh, urdf_str, model)) {
-          ROS_ERROR_STREAM("LayeredHardwareGazebo::init(): Failed to initialize the gazebo layer '"
+        layer = loadGazeboLayer(lookup_name, layer_param_nh, urdf_str, model);
+        if (!layer) {
+          ROS_ERROR_STREAM("LayeredHardwareGazebo::init(): Failed to load the gazebo layer '"
                            << layer_name << "'");
           return false;
         }
         ROS_INFO_STREAM("LayeredHardwareGazebo::init(): Initialized the gazebo layer '"
                         << layer_name << "'");
-        layer = gazebo_layer;
       }
       // load as a non-gazebo layer
       else if (layer_loader_.isClassAvailable(lookup_name)) {
-        // create a layer instance by typename
-        try {
-          layer = layer_loader_.createInstance(lookup_name);
-        } catch (const pluginlib::PluginlibException &ex) {
-          ROS_ERROR_STREAM("LayeredHardwareGazebo::init(): Failed to create a layer instance by "
-                           "the lookup name '"
-                           << lookup_name << "' for the layer '" << layer_name
-                           << "': " << ex.what());
-          return false;
-        }
-        // init the layer
-        if (!layer->init(this, layer_param_nh, urdf_str)) {
-          ROS_ERROR_STREAM("LayeredHardwareGazebo::init(): Failed to initialize the layer '"
-                           << layer_name << "'");
+        layer = loadLayer(lookup_name, layer_param_nh, urdf_str);
+        if (!layer) {
+          ROS_ERROR_STREAM("LayeredHardwareGazebo::init(): Failed to load the layer '" << layer_name
+                                                                                       << "'");
           return false;
         }
         ROS_INFO_STREAM("LayeredHardwareGazebo::init(): Initialized the layer '" << layer_name
@@ -132,45 +94,39 @@ public:
     return true;
   }
 
-  virtual bool prepareSwitch(const std::list< hi::ControllerInfo > &start_list,
-                             const std::list< hi::ControllerInfo > &stop_list) {
-    // ask each layers if stopping/starting given controllers is possible
-    BOOST_REVERSE_FOREACH(const lh::LayerPtr &layer, layers_) {
-      if (!layer->prepareSwitch(start_list, stop_list)) {
-        return false;
-      }
+protected:
+  // override init() from the parent class to prevent calling
+  bool init(const ros::NodeHandle &param_nh) {
+    ROS_ERROR("LayeredHardwareGazebo::init(): Disabled version of init(). Call another version.");
+    return false;
+  }
+
+  GazeboLayerPtr loadGazeboLayer(const std::string &lookup_name, const ros::NodeHandle &param_nh,
+                                 const std::string &urdf_str, const gzp::ModelPtr &model) {
+    GazeboLayerPtr layer;
+
+    // create a gazebo layer instance by typename
+    try {
+      layer = gazebo_layer_loader_.createInstance(lookup_name);
+    } catch (const pluginlib::PluginlibException &ex) {
+      ROS_ERROR_STREAM("LayeredHardwareGazebo::loadGazeboLayer(): Failed to create a gazebo layer  "
+                       "by the lookup name '"
+                       << lookup_name << "': " << ex.what());
+      return GazeboLayerPtr();
     }
-    return true;
-  }
 
-  virtual void doSwitch(const std::list< hi::ControllerInfo > &start_list,
-                        const std::list< hi::ControllerInfo > &stop_list) {
-    // do something required on just before switching controllers
-    BOOST_REVERSE_FOREACH(const lh::LayerPtr &layer, layers_) {
-      layer->doSwitch(start_list, stop_list);
+    // init the layer
+    if (!layer->init(this, param_nh, urdf_str, model)) {
+      ROS_ERROR_STREAM(
+          "LayeredHardwareGazebo::loadGazeboLayer(): Failed to initialize a gazebo layer");
+      return GazeboLayerPtr();
     }
+
+    return layer;
   }
 
-  virtual void read(const ros::Time &time, const ros::Duration &period) {
-    // read states from actuators
-    BOOST_REVERSE_FOREACH(const lh::LayerPtr &layer, layers_) { layer->read(time, period); }
-  }
-
-  virtual void write(const ros::Time &time, const ros::Duration &period) {
-    // write commands to actuators
-    BOOST_FOREACH (const lh::LayerPtr &layer, layers_) { layer->write(time, period); }
-  }
-
-  std::size_t size() const { return layers_.size(); }
-
-  lh::LayerPtr layer(const std::size_t i) { return layers_[i]; }
-
-  lh::LayerConstPtr layer(const std::size_t i) const { return layers_[i]; }
-
-private:
-  pluginlib::ClassLoader< lh::LayerBase > layer_loader_;
+protected:
   pluginlib::ClassLoader< GazeboLayerBase > gazebo_layer_loader_;
-  std::vector< lh::LayerPtr > layers_;
 };
 } // namespace layered_hardware_gazebo
 
